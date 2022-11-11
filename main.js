a8015ea80fdd44111015e95329329bbfafc187d4
danifaso02 <danifaso02@gmail.com>
2022-11-11 16:02:17 +0100

    fine

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 "use strict";
 
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// Dichiarazione variabile
+const { createApp } = Vue
+
+// funzione createApp
+createApp({
+    data() {
+        return {
+            message: 'Hello boys'
+        }
+    }
+}).mount('#title')
