a470d9e8c844c20818d4dbd7cffad264a7a8a508
danifaso02 <danifaso02@gmail.com>
2022-11-11 16:40:09 +0100

    Bonus + commenti

--- main.js
+++ main.js
@@ -9,7 +9,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello boys'
+            title: 'Hi im am Batman',
+
+            // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+            image: {
+                source: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gqitalia.it%2Fshow%2Fcinema%2F2016%2F03%2F04%2Fchristian-bale-batman-non-ho-centrato-lobiettivo-potevo-fare-di-meglio&psig=AOvVaw0calzqXwjoo3kqq8A2RrOd&ust=1668266410744000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjX8-e2pvsCFQAAAAAdAAAAABAE',
+
+                message: 'This is me',
+            }
         }
     }
-}).mount('#title')
+}).mount('#exercise')
+
+
+
